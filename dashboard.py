bdc0dd6a4509d120d57d48b789800a8ffe2403c4
Rainier Ordinario <rainmordi@gmail.com>
2025-03-11 23:33:10 -0700

    initialize

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,17 @@
+# Title: Basic EDA
+# Author: Rainier Ordinario
+# Description: Some Football Exploratory Data Analysis
+
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+# Set layout for page
+st.set_page_config(page_title="Title!!!", page_icon=":bar_chart:",layout="wide")
+
+# Set title
+st.title(" :bar_chart: Sample EDA ")
 
